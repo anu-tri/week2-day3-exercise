cdf6f6bd221527e9f28c52b6ad1e5020466383ad
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2021-10-07 12:19:34 -0700

    jupyter exercise

---
+++ calculate.py
@@ -0,0 +1,10 @@
+from math import pi
+
+def calarea(length, width):
+    area = length * width
+    print(f"Area: {area}")
+          
+def calcircumference(radius):
+    circumference = 2 * pi * radius
+    print(f"Circumference: {circumference}")
+     
